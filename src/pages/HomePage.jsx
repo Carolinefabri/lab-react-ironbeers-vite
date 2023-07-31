cd3e503110aa310c07a45265ad4a27e9c9e073fe
“Caroline <carolinefabri.dev@hotmail.com>
2023-07-31 16:39:48 +0200

    lab done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,56 @@
-function HomePage() {}
+import React from "react";
+import { Link } from "react-router-dom";
+import beersImg from "../assets/beers.png";
+import randomBeerImg from "../assets/random-beer.png";
+import newBeerImg from "../assets/new-beer.png";
+import "../App.css";
+
+function HomePage() {
+  return (
+    <div className="container">
+      {/* All Beers */}
+      <Link to="/beers" className="link">
+        <div className="img-container">
+          <img src={beersImg} alt="beer img" />
+        </div>
+        <h2>All Beers</h2>
+        <p>
+          This section displays a list of all the beers available. Each beer is
+          accompanied by an image, name, and a short tagline. Users can click
+          on a beer to view its details on the dedicated Beer Details page.
+        </p>
+      </Link>
+      {/* Random Beer */}
+      <Link to="/random-beer" className="link">
+        <div className="img-container">
+          <img src={randomBeerImg} alt="beer img" />
+        </div>
+        <h2>Random Beer</h2>
+        <p>
+          This section shows a randomly selected beer from the collection. The
+          displayed beer's details include an image, name, and tagline. Users
+          can click on the beer to view its complete information on the Beer
+          Details page.
+        </p>
+      </Link>
+      {/* New Beer */}
+      <Link to="/new-beer" className="link">
+        <div class="teste">
+        <div className="img-container">
+          <img src={newBeerImg} alt="beer img" />
+        </div>
+        <h2>New Beer</h2>
+        <p>
+          In this section, users can create a new beer. When they click on the
+          "New Beer" link, they will be taken to a page where they can fill out
+          a form to add a new beer to the database. Upon successful submission,
+          a message will be displayed confirming that the new beer has been
+          added to the database.
+        </p></div>
+      </Link>
+      
+    </div>
+  );
+}
 
 export default HomePage;
--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -1,3 +1,60 @@
-function RandomBeersPage() {}
+import axios from "axios";
+import { useState, useEffect } from "react";
+import styled from '@emotion/styled'
 
-export default RandomBeersPage;
+function RandomBeerPage() {
+  const [rdmBeer, setBeer] = useState(null);
+
+  const BeerCard = styled.div`
+  margin : 50px 15vw;
+  padding : 20px ;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  display : flex;
+  flex-direction : column;
+  img{
+    width : 150px;
+    margin : auto;
+  } `
+
+  useEffect(() => {
+    axios
+      .get("https://ih-beers-api2.herokuapp.com/beers")
+      .then((response) => {
+        const nbOfBeers = response.data.length;
+        const rdmBeerNb = Math.floor(Math.random() * nbOfBeers);
+        setBeer(response.data[rdmBeerNb]);
+      })
+      .catch((err) => console.log("Error occurred", err));
+  }, []);
+
+  if (!rdmBeer) return "Loading...";
+
+  return (
+    <div>
+      <BeerCard>
+        <img src={rdmBeer.image_url} alt="beer img" />
+        <h2>{rdmBeer.name}</h2>
+        <h3>{rdmBeer.tagline}</h3>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <p><strong>Attenuation Level:</strong> {rdmBeer.attenuation_level}</p>
+              </td>
+              <td>
+                <p><strong>First Brewed:</strong> {rdmBeer.first_brewed}</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <p>{rdmBeer.description}</p>
+        <h4>Contributed by: {rdmBeer.contributed_by}</h4>
+      </BeerCard>
+    </div>
+  );
+}
+
+export default RandomBeerPage;
